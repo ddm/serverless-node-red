97c7ad5e3d485353b26def055aca7d6290bea2fb
ddm <dimitri.delmarmol@gmail.com>
2023-12-22 00:00:21 +0100

    dff.v

--- main.js
+++ main.js
@@ -37,8 +37,7 @@ const files = {
           functionGlobalContext: {
             runYosys: function (cmd, msg, node) {
               const result = []
-
-              runYosys([ cmd ], [], {
+              runYosys(cmd.split(' '), [], {
                   printLine: line => result.push(line),
                   decodeASCII: true
               }).then(function(){
@@ -65,15 +64,15 @@ const files = {
         "name": "webcontainer-bonus",
         "type": "module",
         "dependencies": {
+          "nodemon": "latest",
           "express": "latest",
           "node-red": "latest",
           "node-red-node-swagger": "latest",
-          "node-red-debugger": "latest",
-          "nodemon": "latest",
           "@yowasp/yosys": "latest"
         },
         "scripts": {
-          "start": "nodemon index.js"
+          "dff": "cat dff.v",
+          "start": "nodemon -x 'node index.js || touch index.js'"
         }
       }`,
     },
@@ -83,6 +82,27 @@ const files = {
       contents: `[{"id":"79f2ac62b7d3a52b","type":"tab","label":"Yosys","disabled":false,"info":"","env":[]},{"id":"d581b155863d86e3","type":"swagger-doc","summary":"","description":"","tags":"yosys","consumes":"","produces":"","parameters":[{"name":"cmd","in":"query","required":true,"type":"string"}],"responses":{"200":{"description":""}},"deprecated":false},{"id":"40795e8e67c44acf","type":"http in","z":"79f2ac62b7d3a52b","name":"","url":"/yosys","method":"get","upload":false,"swaggerDoc":"d581b155863d86e3","x":90,"y":60,"wires":[["9a21494c5a4ba30c"]]},{"id":"760999de666eab31","type":"http response","z":"79f2ac62b7d3a52b","name":"👍","statusCode":"","headers":{},"x":310,"y":180,"wires":[]},{"id":"9a21494c5a4ba30c","type":"function","z":"79f2ac62b7d3a52b","name":"yosys","func":"global.get('runYosys')(msg.req.query.cmd, msg, node)","outputs":1,"timeout":0,"noerr":0,"initialize":"","finalize":"","libs":[],"x":210,"y":120,"wires":[["760999de666eab31"]]}]`
     }
   },
+  'dff.v': {
+    file: {
+      contents: `
+        module dff (
+            q,
+            d,
+            clk
+        );
+
+        output q;
+        reg q;
+        input d;
+        input clk;
+
+        always @(posedge clk) begin: DFF_LOGIC
+            q <= d;
+        end
+
+        endmodule`
+    }
+  }
 }
 
 async function initWebcontainer() {
@@ -94,17 +114,15 @@ async function initWebcontainer() {
 
 async function npm(cmd) {
   const process = await webcontainerInstance.spawn('npm', cmd)
-  const stdout = new WritableStream(OutputStreamPrototype)
 
-  // Capture output
-  process.output.pipeTo(stdout)
+  process.output.pipeTo(new WritableStream(OutputStreamPrototype))
 
-  // Wait for process to exit
   return process.exit
 }
 
 window.addEventListener('load', async () => {
   await initWebcontainer()
+
   webcontainerInstance.on('server-ready', (port, url) => {
     // Ugly hack to avoid hitting the server before it is ready
     if (!initRan) {
@@ -114,5 +132,6 @@ window.addEventListener('load', async () => {
   })
 
   if (await npm(['install']) !== 0) throw new Error('Installation failed')
+  if (await npm(['run', 'dff']) !== 0) throw new Error('dff.v missing')
   if (await npm(['run', 'start']) !== 0) throw new Error('Start failed')
 })
